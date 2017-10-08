f44f98588078623ec96ad0c83a141f1f8751c2db
Jacob Killelea <Jkillelea@protonmail.ch>
2017-10-08 16:59:53 -0600

    update gamma for the open walled one

--- main.m
+++ main.m
@@ -23,11 +23,13 @@ for i = 1:length(files)
   if strcmp(fname, '400inlb-solid.csv') % solid bar
     phi = gamma .* L / Re;
     J   = 0.5 * pi * (Re^4 - Ri^4);
+    theory_gamma = (torque*Re) ./ (G*J); % theoretical gamma
     plot_title = 'Solid Bar';
   else % slotted bar
     phi = gamma .* L / t;
     b   = 2 * pi * R_avg;  % height of unrolled cross section (t is thickness)
     J   = (1/3) * b * t^3; % b/t = 34.558, so alpha = beta = 1/3
+    theory_gamma = (torque*t) ./ (G*J); % theoretical gamma
     plot_title = 'Slotted Bar';
   end
 
@@ -35,24 +37,40 @@ for i = 1:length(files)
   GJ = torque .* L ./ phi;
   theory_GJ = G * J;
 
-  % theoretical gamma
-  theory_gamma = (torque*Re) ./ (G*J);
+
+
+  % print stuff out
+  disp(['=====', fname, '===='])
+  disp('Torque & Predicted $\gamma$ & Actual $\gamma$ & Predicted GJ & Actual GJ')
+  for i = 10:round(length(torque)/5):length(torque)
+    fprintf('%f & %f & %f & %f & %f \\hline \n', ...
+            torque(i), ...
+            theory_gamma(i), ...
+            gamma(i), ...
+            theory_GJ, ...
+            GJ(i) ...
+    );
+  end
 
   % make some plots
+  lw = 2; % line width
   figure; hold on;
-  plot(phi, gamma*10^6);
-  plot(phi, theory_gamma*10^6);
-  title([plot_title, ', gamma']);
-  xlabel('\phi');
+  plot(torque, gamma*10^6, 'LineWidth', lw);
+  plot(torque, theory_gamma*10^6, 'LineWidth', lw);
+  title([plot_title, ', \gamma']);
+  xlabel('Torque (lb*in)');
   ylabel('\gamma, micro radians');
   legend('Actual Gamma', 'Predicted Gamma');
+  xlim([min(torque), max(torque)]);
+  print([fname, '-gamma'], '-dpng');
 
   figure; hold on;
-  plot(torque, GJ);
-  plot([min(torque), max(torque)], [theory_GJ, theory_GJ]);
+  plot(torque, GJ, 'LineWidth', lw);
+  plot([min(torque), max(torque)], [theory_GJ, theory_GJ], 'LineWidth', lw);
   title([plot_title, ', GJ']);
-  xlabel('\phi');
+  xlabel('Torque (lb*in)');
   ylabel('GJ');
   legend('Actual GJ', 'Predicted GJ');
   xlim([min(torque), max(torque)]);
+  print([fname, '-gj'], '-dpng');
 end
